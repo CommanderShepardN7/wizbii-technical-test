8bab545dc1042e9e8753d2072e3f2bb8761edf11
Aramais <grunge.sektor@gmail.com>
2020-04-15 22:28:21 +0200

    1)"proxy.conf.json" file is created for doing request to wizbii api with the proxy server.
    2)Post request for getting the token is created.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'wizbii-technical-test';
+
+export class AppComponent implements OnInit{
+  private token: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+
+    let body = new URLSearchParams();
+    body.set('username', 'decouverte+2@wizbii.com');
+    body.set('password', 'decouverte');
+    body.set('client_id', 'test');
+    body.set('grant_type', 'password');
+
+    let options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded'})
+    };
+
+    this.http.post("/proxy",body.toString(),options).subscribe(
+      response => {
+        console.log("Token is: " + response['access-token']);
+        this.token = response['access-token'];
+      });
+
+  }
+
+
 }
